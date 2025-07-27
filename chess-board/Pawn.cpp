9acf97493a53b3ea7b25c76b7dd3ec7e4f32dc19
RollandMamaligoi <rmamaligoi@gmail.com>
2025-07-27 16:14:37 +0300

    Black Pawn movement logic

--- chess-board/Pawn.cpp
+++ chess-board/Pawn.cpp
@@ -6,25 +6,40 @@ std::vector<std::pair<int, int>> Pawn::getPossibleMoves(Board& board, int posX, 
 
     std::vector<std::pair<int, int>> moves;
     if (pieceColour == "WHITE") {
-        if (posX + 1 < 8 && posY + 1 >= 0 && board.getPieceAt(posX + 1, posY + 1) != nullptr) {//if Pawn has a piece one square up and left/right of it and is within board boundaries
-            if (board.getPieceAt(posX + 1, posY - 1)->getPieceColour() != pieceColour) { //and that Piece is on the opposing team
-                moves.push_back({ posX - 1, posY + 1 }); //Pawn can take
+        if (posX + 1 < 8 && posY + 1 < 8 && board.getPieceAt(posX + 1, posY + 1) != nullptr) {//if Pawn moves one square up and left/right, is within table boundaries 
+            if (board.getPieceAt(posX + 1, posY + 1)->getPieceColour() != pieceColour) { //and that Piece is on the opposing team
+                moves.push_back({ posX + 1, posY + 1 }); //Pawn can take
             }
         }
         if (posX - 1 >= 0 && posY + 1 < 8 && board.getPieceAt(posX - 1, posY + 1) != nullptr) {
-            if (board.getPieceAt(posX + 1, posY + 1)->getPieceColour() != pieceColour) {
-                moves.push_back({ posX + 1, posY + 1 });
+            if (board.getPieceAt(posX - 1, posY + 1)->getPieceColour() != pieceColour) {
+                moves.push_back({ posX - 1, posY + 1 });
             }
         }
-        if (firstMove && board.getPieceAt(posX, posY + 1) == nullptr) {
+        if (posY + 1 < 8 && board.getPieceAt(posX, posY + 1) == nullptr) {
             moves.push_back({ posX , posY + 1 });
-            if (board.getPieceAt(posX , posY + 2) == nullptr)
+            if (firstMove && board.getPieceAt(posX , posY + 2) == nullptr)
                 moves.push_back({ posX, posY + 2});
         }
-
-
     }
-
+    else {
+        if (posX - 1 >= 0 && posY - 1 >= 0 && board.getPieceAt(posX - 1, posY - 1) != nullptr) {
+            if (board.getPieceAt(posX - 1, posY - 1)->getPieceColour() != pieceColour) {
+                moves.push_back({ posX - 1, posY - 1 });
+            }
+        }
+        if (posX + 1 < 8 && posY - 1 >= 0 && board.getPieceAt(posX + 1, posY - 1) != nullptr) {
+            if (board.getPieceAt(posX + 1, posY - 1)->getPieceColour() != pieceColour) {
+                moves.push_back({ posX + 1, posY - 1 });
+            }
+        }
+        if (posY - 1 >= 0 && board.getPieceAt(posX, posY - 1) == nullptr) {
+            moves.push_back({ posX, posY - 1 });
+            if (firstMove && board.getPieceAt(posX, posY - 2) == nullptr) {
+                moves.push_back({ posX, posY - 2 });
+            }
+        }
+    }
 
 
     return moves;
@@ -39,7 +54,7 @@ void Pawn::showMoves()
 {
     char column = 'a';
     for (int i = 0; i < possibleMoves.size(); i++) {
-        std::cout << (column + possibleMoves[i].first) << possibleMoves[i].second << " ";
+        std::cout << static_cast<char>(column + possibleMoves[i].first) << possibleMoves[i].second + 1 << " ";
     }
 }
 
